178f0864575282daa633004986716a770744cebd
srayoung250 <srayoung250@gmail.com>
2026-01-24 21:52:23 -0700

    2nd run at dice game

--- Dice Game.py
+++ Dice Game.py
@@ -18,33 +18,33 @@ while True:
     else:
         print("Invalid input. Please enter a number between 2 and 4 or Q to Quit. ")
 
-maximum_score = 50
-
+maximum_score = 50 
 player_scores = [0 for _ in range(players)]
 
 while max(player_scores) < maximum_score:
-   
+      
     for player_index in range(players):
+        current_score = 0
         print("\nPlayer", player_index + 1, "turn\n")
         print("Current Score:", player_scores[player_index],"\n")
-        current_score = 0 
-        
+                
         while True:
             should_roll = input("Do you want to roll the dice? (y/n): ").lower()
             if should_roll != "y":
                 break
-            
-            value =roll()
+            value = roll()
             if value == 1:
                 print("You rolled a 1! No points this turn.")
+                current_score = 0
                 break
             else:
                 current_score += value
                 print("You rolled a:", value)
-                
-            print("Your current score is:", current_score)
+                print("Your current score is:", player_scores[player_index] + current_score)
+                continue
             
-        player_scores[player_index] +=  current_score
+            print("Invalid input. Please enter 'y' to roll or 'n' to hold.")
+        player_scores[player_index] += current_score
         print("Player", player_index + 1, "total score is:", player_scores[player_index])
         
 maximum_score = max(player_scores)
